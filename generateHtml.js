dc101e2f3e5fae0dd6fdb16383a6917fa47ebf17
Your Name <you@example.com>
2021-06-02 18:41:39 -0500

    bug fixes to generate HTML output

--- generateHtml.js
+++ generateHtml.js
@@ -1,27 +1,23 @@
-var cards = []
+var cards = [];
 
 function makeHTML(team) {
-    // make html hereee
+  // make html hereee
 
-    for(let i=0; i < team.length; i++) {
-        
-        if(team[i].title === "Manager"){
-            managerCard(team[i])
-        }
-        if(team[i].title === "Engineer") {
-            engineerCard(team[i])
-        }
-
-        if(team[i].title === "Intern") {
-            internCard(team[i])
-        }
+  for (let i = 0; i < team.length; i++) {
+    console.log(team[i]);
+    if (team[i].title === "Manager") {
+      managerCard(team[i]);
+    }
+    if (team[i].title === "Engineer") {
+      engineerCard(team[i]);
     }
 
+    if (team[i].title === "Intern") {
+      internCard(team[i]);
+    }
+  }
 
-
-
-
-    const html = `<!DOCTYPE html>
+  const html = `<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -31,41 +27,51 @@ function makeHTML(team) {
         
     </head>
     <body>
-        ${cards}
+        ${cards.join("")}
     </body>
-    </html>`
+    </html>`;
 
-    return html
-};
+  return html;
+}
 
-function managerCard (manager) {
-    var card = `
+function managerCard(manager) {
+  var card = `
     <div>
         <h1>Name: ${manager.name}</h1>
-    </div>
-    `
-    cards.push(card)
-};
+            <h2>Title: ${manager.title}</h2>
+            <h3>ID: ${manager.id}</h3>
+            <h3>Email: ${manager.email}</h3>
+            <h3>Office #: ${manager.officeNumber}</h3>
 
-function internCard (intern) {
-    var card = `
+    </div>
+    `;
+  cards.push(card);
+}
+
+function internCard(intern) {
+  var card = `
     <div>
         <h1>Name: ${intern.name}</h1>
+        <h2>Title: ${intern.title}</h2>
+        <h3>ID: ${intern.id}</h3>
+        <h3>Email: ${intern.email}</h3>
+        <h3>School: ${intern.school}</h3>
     </div>
-    `
-    cards.push(card)
-};
+    `;
+  cards.push(card);
+}
 
-function engineerCard (engineer) {
-    var card = `
+function engineerCard(engineer) {
+  var card = `
     <div>
         <h1>Name: ${engineer.name}</h1>
+        <h2>Title: ${engineer.title}</h2>
+        <h3>ID: ${engineer.id}</h3>
+        <h3>Email: ${engineer.email}</h3>
+        <h3>Github: ${engineer.github}</h3>
     </div>
-    `
-    cards.push(card)
-};
-
-
-
+    `;
+  cards.push(card);
+}
 
 module.exports = makeHTML;
--- index.js
+++ index.js
@@ -5,184 +5,208 @@ const employee = require("./lib/employee");
 const engineer = require("./lib/engineer");
 const manager = require("./lib/manager");
 const intern = require("./lib/intern");
-const generateHtml = require('./generateHtml')
+const generateHtml = require("./generateHtml");
 const teamMembers = [];
 
 function init() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message: "What is your team name?",
-            name: "teamName"
-        },
-        {
-            type: "input",
-            message: "What is your manager Name?",
-            name: "managerName"
-        },
-        {
-            type: "input",
-            message: "what is your manager ID?",
-            name: "managerID",
-        },
-        {
-            type: "input",
-            message: "what is your email address?",
-            name: "managerEmail",
-        },
-        {
-            type: "input",
-            message: "What is your office number?",
-            name: "managerOffice",
-        },
-       
-    ]).then(function(answers) {
-        console.log('answerssss', answers)
-        var newManager = new manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOffice)    
-        teamMembers.push(newManager);
-        addAnother()
-    })
-};
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your team name?",
+        name: "teamName",
+      },
+      {
+        type: "input",
+        message: "What is your manager Name?",
+        name: "managerName",
+      },
+      {
+        type: "input",
+        message: "what is your manager ID?",
+        name: "managerID",
+      },
+      {
+        type: "input",
+        message: "what is your email address?",
+        name: "managerEmail",
+      },
+      {
+        type: "input",
+        message: "What is your office number?",
+        name: "managerOffice",
+      },
+    ])
+    .then(function (answers) {
+      console.log("answerssss", answers);
+      var newManager = new manager(
+        answers.managerName,
+        answers.managerID,
+        answers.managerEmail,
+        answers.managerOffice
+      );
+      teamMembers.push(newManager);
+      addAnother();
+    });
+}
 
 function addAnother() {
-    //prompt do u wanna add another emplye y or n
-    //{
-      //  type: "confirm",
-       // message: "Would u like to add more emplyees?",
-        //name: "addMore"
-    inquirer.prompt([
-        {
+  //prompt do u wanna add another emplye y or n
+  //{
+  //  type: "confirm",
+  // message: "Would u like to add more emplyees?",
+  //name: "addMore"
+  inquirer
+    .prompt([
+      {
         type: "confirm",
         message: "Do you want to add another employee?",
         name: "addAnother",
-        },
-    ]).then(function(answers) {
-        if(answers.addAnother){
-            pickEmployeeType()
-        }
-        else{
-            console.log(teamMembers)
-            const fakeHtml = generateHtml(teamMembers)
-            console.log('fake html!!', fakeHtml)
-            fs.writeFile('team.html', fakeHtml, function(err) {
-
-            })
-        }
-    })
-};
-
+      },
+    ])
+    .then(function (answers) {
+      if (answers.addAnother) {
+        pickEmployeeType();
+      } else {
+        console.log(teamMembers);
+        const fakeHtml = generateHtml(teamMembers);
+        console.log("fake html!!", fakeHtml);
+        fs.writeFile("team.html", fakeHtml, function (err) {});
+      }
+    });
+}
 
 function pickEmployeeType() {
-    //inquiere with list type - pick form intern or engineer
-    inquirer.prompt([
-        {
-            type: "list",
-            message: "What employee do you want to select?",
-            name: "pickEmployeeType",
-            choices: ["intern", "engineer", "manager"],
-        },
-    ]).then(function(answers) {
-        if(answers.pickEmployeeType === "intern"){
-            addIntern()
-        }
-        else{
-            addEngineer(), addManager()
-        }
-    })
+  //inquiere with list type - pick form intern or engineer
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What employee do you want to select?",
+        name: "pickEmployeeType",
+        choices: ["intern", "engineer", "manager"],
+      },
+    ])
+    .then(function (answers) {
+      if (answers.pickEmployeeType === "intern") {
+        addIntern();
+      } else if (answers.pickEmployeeType === "engineer") {
+        addEngineer();
+      } else if (answers.pickEmployeeType === "manager") {
+        addManager();
+      }
+    });
 }
 
 function addEngineer() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Name",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "id:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "email:",
-        },
-        {
-            type: "input",
-            name: "github",
-            message: "github:",
-        }
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "id:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "email:",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "github:",
+      },
     ])
-    .then(function(answers){
-        var newEngineer = new engineer(answers.name, answers.id, answers.email, answers.github)    
-        teamMembers.push(newEngineer);
-        addAnother()
-    })
+    .then(function (answers) {
+      var newEngineer = new engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      teamMembers.push(newEngineer);
+      addAnother();
+    });
 }
 
 function addIntern() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Name",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "id:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "email:",
-        },
-        {
-            type: "input",
-            name: "school",
-            message: "school:",
-        }
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "id:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "email:",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "school:",
+      },
     ])
-    .then(function(answers){
-        var newintern = new intern(answers.name, answers.id, answers.email, answers.school)    
-        teamMembers.push(newintern);
+    .then(function (answers) {
+      var newintern = new intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      teamMembers.push(newintern);
 
-        addAnother()
-    })
+      addAnother();
+    });
 }
 
 function addManager() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "name",
-            message: "Name",
-        },
-        {
-            type: "input",
-            name: "id",
-            message: "id:",
-        },
-        {
-            type: "input",
-            name: "email",
-            message: "email:",
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "Office Number:",
-        }
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "id:",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "email:",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "Office Number:",
+      },
     ])
-    .then(function(answers){
-        var newintern = new intern(answers.name, answers.id, answers.email, answers.officeNumber)    
-        teamMembers.push(newintern);
+    .then(function (answers) {
+      var newManager = new manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamMembers.push(newManager);
 
-        addAnother()
-    })
+      addAnother();
+    });
 }
 
-
 //addEngineer ()
-init()
+init();
